12e3c7fb2fb24940d182400bbdcb2997acd4592c
nbqadri <63450344+nbqadri@users.noreply.github.com>
2023-08-07 11:18:12 +0000

    second actual change

---
+++ member.js
@@ -0,0 +1,46 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var memberSkills = document.getElementById("memberSkills");
+  var memberSkillsList = document.getElementById("memberSkillsList");
+  var memberSkillsListItems = document.getElementsByClassName(
+    "memberSkillsListItem"
+  );
+  var memberSkillsListItemsLength = memberSkillsListItems.length;
+  var memberSkillsListItemsWidth = memberSkillsListItems[0].offsetWidth;
+  var memberSkillsListItemsWidthTotal =
+    memberSkillsListItemsWidth * memberSkillsListItemsLength;
+  var memberSkillsListItemsWidthTotalHalf = memberSkillsListItemsWidthTotal / 2;
+  var memberSkillsListItemsWidthTotalHalfNegative =
+    memberSkillsListItemsWidthTotalHalf * -1;
+  var memberSkillsListItemsWidthTotalHalfNegativeString =
+    memberSkillsListItemsWidthTotalHalfNegative + "px";
+  var memberSkillsListItemsWidthTotalString =
+    memberSkillsListItemsWidthTotal + "px";
+  var memberSkillsListItemsWidthTotalHalfString =
+    memberSkillsListItemsWidthTotalHalf + "px";
+  var memberSkillsListItemsWidthTotalHalfNegativeString =
+    memberSkillsListItemsWidthTotalHalfNegative + "px";
+  var memberSkillsListItemsWidthTotalHalfNegativeString =
+    memberSkillsListItemsWidthTotalHalfNegative + "px";
+  var memberSkillsListItemsWidthTotalHalfNegativeString =
+    memberSkillsListItemsWidthTotalHalfNegative + "px";
+  var memberSkillsListItemsWidthTotalHalfNegativeString =
+    memberSkillsListItemsWidthTotalHalfNegative + "px";
+  var memberSkillsListItemsWidthTotalHalfNegativeString =
+    memberSkillsListItemsWidthTotalHalfNegative + "px";
+  var memberSkillsListItemsWidthTotalHalfNegativeString =
+    memberSkillsListItemsWidthTotalHalfNegative + "px";
+  var memberSkillsListItemsWidthTotalHalfNegativeString =
+    memberSkillsListItemsWidthTotalHalfNegative + "px";
+  var memberSkillsListItemsWidthTotalHalfNegativeString =
+    memberSkillsListItemsWidthTotalHalfNegative + "px";
+  var memberSkillsListItemsWidthTotalHalfNegativeString =
+    memberSkillsListItemsWidthTotalHalfNegative + "px";
+
+  memberSkillsList.style.width = memberSkillsListItemsWidthTotalString;
+
+  memberSkillsList.style.left = memberSkillsListItemsWidthTotalHalfNegativeString;
+
+  memberSkillsList.style.transition = "all 0.5s ease";
+
+  }
